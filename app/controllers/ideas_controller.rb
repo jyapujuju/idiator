44423db424ce2acdd80de676efe0480b871a0134
jyapujuju <danieldangol@gmail.com>
2016-09-21 13:35:29 +0545

    adding links

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -8,12 +8,21 @@ class IdeasController < ApplicationController
   end
 
   def edit
-    @ideas = Idea.find(params[:id])
+    @idea = Idea.find(params[:id])
+  end
+
+  def update
     if @idea.update(idea_params)
       redirect_to root_path
     else
       redirect_to edit_idea_path(params[:id])
     end
+  end
+
+  def destroy
+    @idea = Idea.find(params[:id])
+    @idea.destroy
+    redirect_to root_path
   end
 
   def update
