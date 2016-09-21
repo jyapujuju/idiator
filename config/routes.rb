de11588718f34640dd7cae0f8e8a0eb900e35406
jyapujuju <danieldangol@gmail.com>
2016-09-21 14:35:37 +0545

    random page

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   root 'ideas#index'
   resources :ideas
   get 'about' => 'static_pages#about'
-  get 'random' => 'static_page#random'
+  get 'random' => 'static_pages#random'
 end
